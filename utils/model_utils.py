53ad52da6021726ffb4ba4d99d28b2b6c46a1b97
Junxuan Liang <148680729+Dreamer-Vitalis@users.noreply.github.com>
2024-11-15 11:46:23 +0800

    Update model_utils.py

--- utils/model_utils.py
+++ utils/model_utils.py
@@ -17,32 +17,32 @@ def get_model_list(target_model):
             models[model] = []
             models[model].append((112, 112))
             fr_model = ir152.IR_152((112, 112))
-            fr_model.load_state_dict(torch.load('./assets/models/ir152.pth'))
-            fr_model.to("cuda")
+            fr_model.load_state_dict(torch.load('./assets/models/ir152.pth', map_location=torch.device('cpu')))
+            fr_model.to("cpu")
             fr_model.eval()
             models[model].append(fr_model)
         if model == 'irse50':
             models[model] = []
             models[model].append((112, 112))
             fr_model = irse.Backbone(50, 0.6, 'ir_se')
-            fr_model.load_state_dict(torch.load('./assets/models/irse50.pth'))
-            fr_model.to("cuda")
+            fr_model.load_state_dict(torch.load('./assets/models/irse50.pth', map_location=torch.device('cpu')))
+            fr_model.to("cpu")
             fr_model.eval()
             models[model].append(fr_model)
         if model == 'facenet':
             models[model] = []
             models[model].append((160, 160))
-            fr_model = facenet.InceptionResnetV1(num_classes=8631, device="cuda")
-            fr_model.load_state_dict(torch.load('./assets/models/facenet.pth'))
-            fr_model.to("cuda")
+            fr_model = facenet.InceptionResnetV1(num_classes=8631, device="cpu")
+            fr_model.load_state_dict(torch.load('./assets/models/facenet.pth', map_location=torch.device('cpu')))
+            fr_model.to("cpu")
             fr_model.eval()
             models[model].append(fr_model)
         if model == 'mobile_face':
             models[model] = []
             models[model].append((112, 112))
             fr_model = irse.MobileFaceNet(512)
-            fr_model.load_state_dict(torch.load('./assets/models/mobile_face.pth'))
-            fr_model.to("cuda")
+            fr_model.load_state_dict(torch.load('./assets/models/mobile_face.pth', map_location=torch.device('cpu')))
+            fr_model.to("cpu")
             fr_model.eval()
             models[model].append(fr_model)
     return models
